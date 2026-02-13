55a8e20abca2867bb2fe5f7ddaac3ec44c48da76
KavyaJP <kavya31052005@gmail.com>
2026-02-13 16:09:01 +0530

    Added Jump State to player

--- hybrid-2d-3d-demo/scenes/player/player.gd
+++ hybrid-2d-3d-demo/scenes/player/player.gd
@@ -1,7 +1,7 @@
 class_name Player
 extends CharacterBody3D
 
-@export var speed : float = 2
-@export var jump_velocity : float = 1.5
+@export var speed : float = 5
+@export var jump_velocity : float = 4.5
 
 var direction: Vector3
